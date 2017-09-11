92c49c78b09c9a869964678349175397caaabaaf
WheresWaldo <bud.hammerton@gmail.com>
2017-09-11 10:39:14 -0400

    Added comment

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -534,7 +534,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 145.5 }  //[BH]
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 145.0 }  //[BH]
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -859,7 +859,7 @@
 //#define AUTO_BED_LEVELING_LINEAR
 //#define AUTO_BED_LEVELING_BILINEAR
 #define AUTO_BED_LEVELING_UBL  //[BH]
-//#define MESH_BED_LEVELING
+//#define MESH_BED_LEVELING  //[BH]
 
 /**
  * Enable detailed logging of G28, G29, M48, etc.
