0b6d09751b3a01bee2dcd98e238f75f9f5048094
WheresWaldo <bud.hammerton@gmail.com>
2019-02-11 14:17:10 -0500

    Fixed typo
    Fixed a missed comment resulting in not allowing the Z Offset to be set below the trigger point.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -878,7 +878,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 258  //[BH]
+#define Z_MAX_POS 255  //[BH]
 
 /**
  * Software Endstops
@@ -898,7 +898,7 @@
 #endif
 
 // Max software endstops constrain movement within maximum coordinate bounds
-#define MAX_SOFTWARE_ENDSTOPS
+//#define MAX_SOFTWARE_ENDSTOPS  [BH]
 #if ENABLED(MAX_SOFTWARE_ENDSTOPS)
   #define MAX_SOFTWARE_ENDSTOP_X
   #define MAX_SOFTWARE_ENDSTOP_Y
